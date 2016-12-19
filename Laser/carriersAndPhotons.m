e1c93083333bd6c12dfa5c2ef56f292530c931e2
G <guillemviladrich@hotmail.com>
2016-12-19 21:14:28 +0100

    canvis pre trucada

--- Laser/carriersAndPhotons.m
+++ Laser/carriersAndPhotons.m
@@ -1,7 +1,7 @@
-function [ S, N ] = carriersAndPhotons( v,Gamma,N0,alfat,thaur,a,I,V,beta,t,delta )
+function [ S, N ] = carriersAndPhotons( v,Gamma,N0,alfat,thaur,a,I,V,beta,t,delta,Nth,S0 )
 q=1.6*10^(-19);
-S=ones(1,length(t));
-N=ones(1,length(t));
+S=S0*ones(1,length(t));
+N=Nth*ones(1,length(t));
 for i=2:length(t)
     S(i)=S(i-1)+(v*Gamma*a*(N(i-1)-N0)*S(i-1)-v*alfat*S(i-1)+beta*N(i-1)/thaur)*delta;
     N(i)=N(i-1)+(I(i)/(q*V)-N(i-1)/thaur-v*Gamma*a*(N(i-1)-N0)*S(i-1))*delta;
